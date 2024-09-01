095b609c1d5a2a527250064970d160aa9c376b40
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-09-01 16:00:27 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioTecnico/VerificaString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaString
+{
+    internal class VerificaString
+    {
+        static int CountAInString(string palavra)
+        {
+            int count = 0;
+            foreach (char c in palavra.ToLower())
+            {
+                if (c.Equals("a")){
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main()
+        {
+            Console.Write("Informe uma palavra: ");
+            string input = Console.ReadLine();
+
+            int count = CountAInString(input);
+
+            if (count > 0)
+                Console.WriteLine($"A letra 'a' aparece {count} vezes na palavra.");
+            else
+                Console.WriteLine("A letra 'a' não aparece na palavra.");
+        }
+    }
+}
---
+++ VerificacaoSequencia/Sequencia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequencia
+{
+    internal class Sequencia
+    {
+        static bool IsFibonacci(int num)
+        {
+            if (num < 0)
+                return false;
+
+            int verificador1 = 0, verificador2 = 1;
+            while (verificador1 < num)
+            {
+                int temp = verificador1;
+                verificador1 = verificador2;
+                verificador2 = temp + verificador2;
+            }
+            return verificador1 == num;
+        }
+
+        static void Main()
+        {
+            Console.Write("Informe um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (IsFibonacci(numero))
+                Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci. :)");
+            else
+                Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci. :(");
+        }
+    }
+}
